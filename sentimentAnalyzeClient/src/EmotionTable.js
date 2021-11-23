becb679855bed8e786ccee125fffc487ada06dc5
Iosif Osman <iosif.osman@gmail.com>
2021-11-23 13:36:34 +0000

    adding corrected emotion table

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -2,25 +2,21 @@ import React from 'react';
 import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
-    state = {emotionList:[]}
     render() {
-        let mappedEmotionList = Object.entries(this.props.emotions).map(function(mapentry) {
-            return (
-                <tr>
-                <td>{mapentry[0]}</td>
-                <td>{mapentry[1]}</td>
-                </tr>
-            )
-            })  
-            this.setState({emotionList: mappedEmotionList})
-
         //Returns the emotions as an HTML table
       return (  
         <div>
           <table className="table table-bordered">
             <tbody>
             {
-                this.state.emotionList
+                Object.entries(this.props.emotions).map(function(mapentry) {
+                    return (
+                    <tr>
+                    <td>{mapentry[0]}</td>
+                    <td>{mapentry[1]}</td>
+                    </tr>
+                )
+            })  
             }
             </tbody>
           </table>
